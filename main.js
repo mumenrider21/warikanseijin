2313263549cb7e5e7288e9a0fa32eefaabba60c2
無免ライダー <106133707+mumenrider21@users.noreply.github.com>
2022-12-08 10:07:56 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("main.js!!");
+
+$(document).ready(()=>{
+	console.log("Ready!!");
+	
+
+	alert("割り勘でいいか？　ウルトラマン")
+
+	console.log("	案件:「割り勘」を計算するアプリを作る事要件: 金額入力、人数入力を入力する事で、割り勘金額が算出される条件: 各チームごと開発を進める事(CEOは全員提出を確認) 提出: GitHub PagesにUploadし、URLを報告する事 期限: 12月19日(月)")
+});
